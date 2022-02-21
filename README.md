341cccc71c990f04244e962343338698406a3e15
Zach Estreito <zachestreito@gmail.com>
2022-02-20 21:36:45 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# nbgrader_moss
+
+### Usage
+* Replace the USER_ID constant at the top with your Stanford Moss user id.
+* Call the check() function with one or two arguments.
+* Note: Single argument execution will assume nbgrader_moss.py is inside the course directory.
+
+check(*assignment_name*, *course_directory = None*)
+
+### Example command line usage:
+```
+python3 -c "import nbgrader_moss; nbgrader_moss.check('assignment1', '/home/user/CS426')"
+```
---
+++ nbgrader_moss.py
@@ -0,0 +1,84 @@
+import sys, os, stat, urllib.request, nbconvert
+from os.path import exists
+from pathlib import Path
+from nbgrader.apps import NbGraderAPI
+from traitlets.config import Config
+
+course_dir = None
+
+def check(*arg): # first arg = assignment name, second OPTIONAl arg = custom course dir
+    # process args
+    if len(arg) == 1:
+        assignment_name = arg[0].replace(" ", "_")
+        course_dir = (os.getcwd())
+    elif len(arg) == 2:
+        assignment_name = arg[0].replace(" ", "_")
+        course_dir = arg[1]
+        os.chdir(course_dir) # change working directory for os commands
+    else:
+        print("Error: Incorrect number of arguments")
+        return
+
+    # create api config
+    config = Config()
+    config.CourseDirectory.root = course_dir
+
+    # initialize api with config
+    nb_api = NbGraderAPI(config=config)
+
+    # check for moss script, download if not found
+    if not exists("%s/moss.pl" % course_dir):
+        print("moss.pl not found.")
+        print("Do you have a moss account (Y/n)?")
+        moss_in = input()
+        if moss_in.lower() == "n":
+            print("You can find Moss registration instructions here: http://moss.stanford.edu/")
+            return
+        elif (moss_in[0].lower() == "y" or moss_in == ""):
+            user_id = input("Enter Moss UserID: ")
+            if (len(user_id) != 9 or not user_id.isdecimal()):
+                print("Invalid Moss UserID. Exiting.")
+                return
+            else:
+                url = "http://moss.stanford.edu/general/scripts/mossnet"
+                urllib.request.urlretrieve(url, "moss.pl")
+                file = Path("%s/moss.pl" % course_dir)
+                file.chmod(file.stat().st_mode | stat.S_IEXEC) # make moss.pl executable
+                with open(file, 'r+') as f:
+                    r = f.read().replace('987654321', user_id) # replace user id in moss.pl with input
+                    f.seek(0)
+                    f.write(r)
+        else:
+            print("Invalid input. Exiting.")
+            return
+
+    # create set of students who have submitted assignment_name
+    student_set = nb_api.get_submitted_students(assignment_name)
+    if len(student_set) <= 0: # terminate if no submissions found
+        print("Error: No submissions found for %s" % assignment_name)
+        return
+
+    # create directory for converted files
+    os.makedirs("moss/%s/submissions/" % assignment_name, exist_ok=True)
+
+    # convert base file
+    __convert("%s/release/%s/%s.ipynb" % (course_dir, assignment_name, assignment_name), "%s/moss/%s/" % (course_dir, assignment_name), "base")
+
+    # convert student submissions
+    for student in student_set:
+        __convert("%s/submitted/%s/%s/%s.ipynb" % (course_dir, student, assignment_name, assignment_name), "%s/moss/%s/submissions/" % (course_dir, assignment_name), student)
+
+    # SUBMIT!
+    __submit(course_dir, assignment_name)
+
+
+# convert .ipynb assignments to trimmed .p
+def __convert(input_file, output_dir, output_name):
+    os.system("jupyter nbconvert --output='%s%s' --to python %s" % (output_dir, output_name, input_file))
+    return
+
+
+# submit files to moss
+def __submit(course_dir, assignment_name):
+    command = ("%s/moss.pl -l python -b %s/moss/%s/base.py -d %s/moss/%s/submissions/*.py" % (course_dir, course_dir, assignment_name, course_dir, assignment_name))
+    os.system(command)
